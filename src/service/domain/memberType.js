4c83b1695210ce15fa36010be2ce22049ea91146
Mino <mino@nextunicorn.kr>
2023-05-12 03:06:50 +0900

    chore: remove build js files

--- src/service/domain/memberType.js
+++
@@ -1,5 +0,0 @@
-var MemberType;
-(function (MemberType) {
-    MemberType[MemberType["Owner"] = 1] = "Owner";
-    MemberType[MemberType["Memeber"] = 2] = "Memeber";
-})(MemberType || (MemberType = {}));
